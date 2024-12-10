f670c166c416c4cf0e028a26216759f5c76bdf04
Nick <blaauwnicholas@gmail.com>
2024-12-10 15:37:59 -0600

    -clarified comments -changed input.txt to unsorted.txt -changed output.txt to sorted.txt

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,17 @@ fn main() {
     println!("Generating Numbers to Sort");
     let mut rng = thread_rng();
 
-    //creates a file of 498 unsorted ints
-    //feel free to comment this block out if you have a custom list of ints needing to be sorted
+    //creates a file of 500 unsorted integers, each ranging from 1-500 inclusive
+    //feel free to comment this block out (lines 15-20 inclusive) if you have a custom list of integers that need to be sorted
     let mut input_file: fs::File = fs::File::create("unsorted.txt").expect("Could not create file");
-    for _i in 1..500 {
-        //generates an int and writes it to a new line in the unsorted.txt file
+    for _i in 0..500 {
+        //generates an integer and writes it to a new line in the unsorted.txt file
         write!(input_file, "{}\n", rng.gen_range(1..=500)).expect("Could not write to file");
     }
     println!("List Generated");
 
 
-    //reads a file of unsorted ints
+    //reads a file of unsorted integers
     let mut sorting_list:Vec<i32> = Vec::new();
     for line in fs::read_to_string("unsorted.txt").unwrap().lines(){
         //converts each line (currently a string slice) to a interger and adds it to the vector of nums to sort
@@ -29,13 +29,13 @@ fn main() {
 
     println!("Unsorted file created");
 
-    //sorts the nums
+    //sorts the integers
     quicksort::sort(&mut sorting_list);
 
     //creates an output file
-    let mut output_file: fs::File = fs::File::create("output.txt").expect("Could not create file");
+    let mut output_file: fs::File = fs::File::create("sorted.txt").expect("Could not create file");
     
-    //writes the sorted ints to the file each on an individual line
+    //writes the sorted integers to the file each on an individual line
     for num in &sorting_list{
         write!(output_file, "{}\n", num).expect("Could not write to file");
     }
